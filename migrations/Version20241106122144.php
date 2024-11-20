52ac280117afe6944a6fb448472dfb5a414d000f
MarsiQueen <marmanica@alu.edu.gva.es>
2024-11-20 12:22:51 +0100

    Plantilla implementada, creación de la estructura de las páginas

--- migrations/Version20241106122144.php
+++ migrations/Version20241106122144.php
@@ -22,7 +22,7 @@ final class Version20241106122144 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE comment (id INT AUTO_INCREMENT NOT NULL, user_id INT DEFAULT NULL, post_id INT DEFAULT NULL, content VARCHAR(255) NOT NULL, datetime DATETIME NOT NULL, INDEX IDX_9474526CA76ED395 (user_id), INDEX IDX_9474526C4B89032C (post_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE comment ADD CONSTRAINT FK_9474526CA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
-        $this->addSql('ALTER TABLE comment ADD CONSTRAINT FK_9474526C4B89032C FOREIGN KEY (post_id) REFERENCES post (id)');
+        //$this->addSql('ALTER TABLE comment ADD CONSTRAINT FK_9474526C4B89032C FOREIGN KEY (post_id) REFERENCES post (id)');
     }
 
     public function down(Schema $schema): void
--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -8,10 +8,35 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PageController extends AbstractController
 {
-    #[Route('/', name: 'app_page')]
+    #[Route('/', name: 'home')]
     public function index(): Response
     {
         return $this->render('page/index.html.twig', [
+            'page_title' => 'Home',
+        ]);
+    }
+
+    #[Route('/explore', name: 'explore')]
+    public function explore(): Response
+    {
+        return $this->render('page/explore.html.twig', [
+            'page_title' => 'Explore',
+        ]);
+    }
+
+    #[Route('/profile', name: 'profile')]
+    public function profile(): Response
+    {
+        return $this->render('page/profile.html.twig', [
+            'page_title' => 'Profile',
+        ]);
+    }
+
+    #[Route('/chats', name: 'chats')]
+    public function chats(): Response
+    {
+        return $this->render('page/chats.html.twig', [
+            'page_title' => 'Chats',
         ]);
     }
 }
